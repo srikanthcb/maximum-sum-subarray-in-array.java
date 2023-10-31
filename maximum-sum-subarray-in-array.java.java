f35e14dcd6818811ed805a154d144bf104db53d9
srikanthcb <110121654+srikanthcb@users.noreply.github.com>
2023-10-31 14:16:37 +0530

    Add files via upload

---
+++ maximum-sum-subarray-in-array.java.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+            if (currSum > maxSum) {
+                maxSum = currSum;
+            }
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+        System.out.println("Maximum sum subarray: " + maxSum);
+    }
+}
+
+/* output:
+      Maximum sum subarray: 6   */
